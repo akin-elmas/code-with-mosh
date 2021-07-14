1aa1624d3391b2122da51664395a30b79400bc71
Akın Elmas <36002989+akin-elmas@users.noreply.github.com>
2021-07-14 18:41:42 +0300

    Delete 2 directory

--- 2/lp.js
+++
@@ -1,4 +0,0 @@
-console.log(isLandscape(500, 200));
-function isLandscape(width, height) {
-  return width > height;
-}
--- 2/maxoftwonum.js
+++
@@ -1,6 +0,0 @@
-let number = max(1, 2);
-console.log(number);
-function max(a, b) {
-  if (a > b) return a;
-  else return b;
-}
